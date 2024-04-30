fde5c374fd8047016eb012b447cf921c41f7f724
pattar-314 <patsdroid19@gmail.com>
2024-04-30 04:46:29 -0600

    on to 3b

--- part3/phoneBackend/index.js
+++ part3/phoneBackend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const axios = require("axios")
+const morgan = require("morgan")
 
 const app = express()
 
@@ -27,17 +28,52 @@ let persons = [
 ]
 
 app.use(express.json())
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/', (request, response) => {
+app.get('/info', (reqest, response) => {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  const dateNow = new Date()
+
+  console.log(dateNow.toISOString())
+
+  response.send(`<div><p>Phonebook has info for ${persons.length} people</p><p>${dateNow.toLocaleString(undefined, options)}  ${dateNow.toTimeString()}</p></div>`)   
 
 })
 
-app.get('/api/persons/:id', (request, response) => response.json(persons.filter((p) => p.id === Number(request.params.id))))
+app.get('/api/persons/:id', (request, response) => {
+  const foundPerson = persons.filter((p) =>  p.id === Number(request.params.id))
+  if(foundPerson.length < 1){
+    console.log('could not find person with that id')
+    response.status(404).json({error: 'person not found'})
+  } else {
+    response.json(foundPerson[0])
+  }
+})
 
 app.get('/api/persons', (request, response) => response.json(persons))
 
 app.post('/api/persons', (request, response) => {
-  persons = persons.concat(request.body)
+  const body = request.body
+  if(!body.name || !body.number){
+    response.status(402).json({error: 'requires name and number'})
+    return null
+  } else if(persons.filter((p) => p.name === body.name).length > 0){
+    response.status(402).json({error: 'duplicate name'})
+    return null
+  }
+  const newPerson = {...body, id: Math.floor(Math.random() * 10000 )}
+  console.log('new person: ', newPerson)
+  persons = persons.concat(newPerson)
+  if(response.status === 404){
+    console.log('could not add person')
+    response.json({error: 'could not add person'})
+  }
   response.json(persons)
 })
 
@@ -47,6 +83,10 @@ app.delete('/api/persons/:id', (request, response) => {
   console.log('newList:', newList)
   persons = newList
   response.json(persons)
+})
+
+app.get('/', (request, response) => {
+
 })
 
 const PORT = 3001
