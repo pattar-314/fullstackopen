d32df468cf6b177e64c415c4549bb89c49cf04a0
pattar-314 <patsdroid19@gmail.com>
2024-12-09 02:50:39 -0700

    end of lunch 12-9

--- part7/blogApp-redux/frontend/src/App.jsx
+++ part7/blogApp-redux/frontend/src/App.jsx
@@ -6,6 +6,8 @@ import './main.css'
 import { setUser } from './reducers/userReducer'
 import { useDispatch, useSelector } from 'react-redux'
 import { setNotification } from './reducers/notificationReducer'
+import AllUsers from './components/AllUsers'
+import SingleUser from './components/SingleUser'
 
 const App = () => {
   const dispatch = useDispatch()
@@ -43,11 +45,19 @@ const App = () => {
     
   }
 
+  const idMAtch = useMatch('/:id')
+
 
   return (
     <div className='app-wrapper'>
       {!notification ? null : <div className={`notification ${notification.status}`}><b>{notification.message}</b></div> }
-      {user ? <BlogMain user={user} handleNotification={handleNotification} /> : <LoginForm handleLogin={handleLogin} handleNotification={handleNotification} /> }
+      <Routes>
+        <Route path='/' element={user ? <BlogMain user={user} handleNotification={handleNotification} /> : <LoginForm handleLogin={handleLogin} handleNotification={handleNotification} />} />
+        <Route path='/users' element={<AllUsers />} />
+        <Route path='users/:id' element={<SingleUser />} />
+      </Routes>
+      
+  
     </div>
 
   )
--- part7/blogApp-redux/frontend/src/main.jsx
+++ part7/blogApp-redux/frontend/src/main.jsx
@@ -1,33 +1,33 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 // import store from './services/store'
-import { Provider } from 'react-redux'
-import App from './App'
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router";
+import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
-import blogSlice from './reducers/blogReducer'
-import notificationSlice from './reducers/notificationReducer'
-import userSlice from './reducers/userReducer'
-
+import blogSlice from "./reducers/blogReducer";
+import notificationSlice from "./reducers/notificationReducer";
+import userSlice from "./reducers/userReducer";
 
 const store = configureStore({
   reducer: {
     blogs: blogSlice,
     notification: notificationSlice,
-    user: userSlice
-  }
-})
+    user: userSlice,
+  },
+});
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const renderApp = () => {
-  root.render( 
-  <Provider store={store}>
-    <App />
-  </Provider>
- )
-}
+  root.render(
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  );
+};
 
-renderApp()
+renderApp();
 
-store.subscribe(renderApp)
+store.subscribe(renderApp);
